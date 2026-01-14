531ecd828f9c31359394d100d6c81dc6653d0176
Sebastian Valenzuela <sebastianvalenzuela@mac.lan>
2026-01-14 11:40:53 -0700

    Update FinanceContext

--- intranet/src/context/FinanceContext.tsx
+++ intranet/src/context/FinanceContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, ReactNode } from 'react'
+import { createContext, useContext, useState, type ReactNode } from 'react'
 import type { BankAccount, Transaction, PurchaseInvoice } from '../types/finance'
 
 interface FinanceContextType {
